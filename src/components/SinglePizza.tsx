69c8d0d0a76d3ae687df3ae8605fd515d788167c
Andrey Zuev <stepgt@mail.ru>
2023-04-10 09:22:39 +1000

    Added EditPizzaForm component && Refactor SinglePizza

--- src/components/SinglePizza.tsx
+++ src/components/SinglePizza.tsx
@@ -1,12 +1,17 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import Pizza from '../models/Pizza';
+import EditPizzaForm from './EditPizzaForm';
 
 interface ISinglePizza {
   pizza: Pizza;
 }
 
 const SinglePizza: FC<ISinglePizza> = ({ pizza }) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const handleToggleEdit = () => {
+    setEdit(!edit);
+  };
   return (
     <div className="pizza">
       <img src={`/images/${pizza.img}`} alt={pizza.title} />
@@ -14,9 +19,11 @@ const SinglePizza: FC<ISinglePizza> = ({ pizza }) => {
       <span>{pizza.price} ₽</span>
 
       <div className="pizza-controls">
-        <AiFillEdit onClick={() => {}} />
+        <AiFillEdit onClick={handleToggleEdit} />
         <AiFillDelete onClick={() => {}} />
       </div>
+
+      {edit ? <EditPizzaForm data={pizza} /> : null}
     </div>
   );
 };
